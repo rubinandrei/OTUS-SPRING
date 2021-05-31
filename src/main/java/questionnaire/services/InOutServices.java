8dc2fd56ad64d1dcc50ca7c6f084b42a0ae10fe7
arubin <Andrei_Rubin@bmc.com>
2021-05-31 22:01:26 +0300

    add Questions

---
+++ src/main/java/questionnaire/services/InOutServices.java
@@ -0,0 +1,9 @@
+package questionnaire.services;
+
+public interface InOutServices {
+    void print(String message);
+
+    int readAnswer(int count);
+
+
+}
---
+++ src/test/java/InOutServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import questionnaire.services.InOutServices;
+import questionnaire.services.InOutServicesImpl;
+
+import java.io.ByteArrayInputStream;
+import java.math.BigInteger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class InOutServiceTest {
+
+
+    @Test
+    public void readAnswerTest(){
+        System.setIn(new ByteArrayInputStream(String.valueOf(4).getBytes()));
+        InOutServices services = new InOutServicesImpl();
+        assertThat(services.readAnswer(5) , is(4));
+    }
+
+
+}
