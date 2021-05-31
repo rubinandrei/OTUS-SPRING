69bbf3930533c4aa6d5c75ba4c65bc5798c82b1a
arubin <Andrei_Rubin@bmc.com>
2021-05-31 23:55:03 +0300

    add missing sources

--- src/main/java/questionnaire/dao/CSVDaoImpl.java
+++ src/main/java/questionnaire/dao/CSVDaoImpl.java
@@ -17,19 +17,19 @@ import java.util.stream.Collectors;
 public class CSVDaoImpl implements CSVDao {
     final static Logger LOGGER = Logger.getLogger(CSVDaoImpl.class);
 
-    private final String QUESTION_FILE;
-    private final String ANSWER_FILE;
+    private final String question_file;
+    private final String answer_file;
 
     public CSVDaoImpl(String questionFile, String answerFile) {
-        this.QUESTION_FILE = questionFile;
-        this.ANSWER_FILE = answerFile;
+        this.question_file = questionFile;
+        this.answer_file = answerFile;
     }
 
     @Override
     public List<Question> getQuestions() {
         List<Answer> answers = getAnswer();
         List<Question> questions = new ArrayList<>();
-        try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(QUESTION_FILE);
+        try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(question_file);
              BufferedReader questionReader = new BufferedReader(new InputStreamReader(stream))) {
             questions = questionReader.lines()
                     .map(q -> q.split(";"))
@@ -51,7 +51,7 @@ public class CSVDaoImpl implements CSVDao {
     @Override
     public List<Answer> getAnswer() {
         List<Answer> answers = new ArrayList<>();
-        try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(ANSWER_FILE);
+        try (InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(answer_file);
              BufferedReader questionReader = new BufferedReader(new InputStreamReader(stream))) {
             answers = questionReader.lines()
                     .map(q -> q.split(";"))
