3efd51df8ed35c69b53348fa020608151fd27d33
arubin <Andrei_Rubin@bmc.com>
2021-06-06 13:33:35 +0300

    remove second csv file (with answer) put answers to questions.csv

--- src/main/java/questionnaire/StartQuestions.java
+++ src/main/java/questionnaire/StartQuestions.java
@@ -1,14 +1,19 @@
 package questionnaire;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import questionnaire.services.QuizService;
 
+@ComponentScan
+@Configuration
 public class StartQuestions {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("questionnaire-context.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(StartQuestions.class);
         QuizService quiz = context.getBean(QuizService.class);
         quiz.showQustions();
 
